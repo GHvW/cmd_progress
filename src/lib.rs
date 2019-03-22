ef788c056a0d20a8baf53a52e3731b910f5646f7
Garrett van Wageningen <ghvanw283@gmail.com>
2019-03-21 22:29:24 -0500

    implement and test first iteration

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+use std::io;
+use std::io::prelude::*;
+
+pub fn print_progress(percent: usize) -> io::Result<()> {
+  let num_bars = percent / 5;
+  let num_spaces = 20 - num_bars;
+
+
+  let mut progress = String::new();
+  for _ in 0..num_bars {
+    progress.push('|');
+  }
+
+  for _ in 0..num_spaces {
+    progress.push(' ');
+  }
+
+  // print!("[{}]\r", progress);
+  print!("\r[{}]", progress);
+  io::stdout().flush()?;
+  Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::{ thread, time};
+extern crate cmd_progress_lib;
+use cmd_progress_lib as progress;
+
 fn main() {
     println!("Hello, world!");
+    let _duration = time::Duration::from_millis(1000);
+    let o_duration = time::Duration::from_millis(150);
+
+    // progress::print_progress(30).unwrap();
+    // thread::sleep(duration);
+
+    // progress::print_progress(40).unwrap();
+    // thread::sleep(duration);
+    
+    // progress::print_progress(50).unwrap();
+    // thread::sleep(duration);
+
+    // progress::print_progress(60).unwrap();
+    // thread::sleep(duration);
+
+    // progress::print_progress(100).unwrap();
+    // thread::sleep(duration);
+
+    for i in 0..=100 {
+        progress::print_progress(i).unwrap();
+        thread::sleep(o_duration);
+    }
 }
